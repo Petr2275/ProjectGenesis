f39d97c727e477bfdab6dcbda5f92bd77d6508db
Petr2275 <petrmichalik81@gmail.com>
2025-07-05 14:45:39 +0200

    presu main tridy do Test, aplikace nabjehla

---
+++ src/test/java/com/engeto/Genesis/Resources/GenesisResourcesApplication.java
@@ -0,0 +1,4 @@
+package com.engeto.Genesis.Resources;
+
+public class GenesisResourcesApplication {
+}
--- src/test/java/com/engeto/Genesis/Resources/GenesisResourcesApplicationTests.java
+++ src/test/java/com/engeto/Genesis/Resources/GenesisResourcesApplicationTests.java
@@ -2,8 +2,7 @@ package com.engeto.Genesis.Resources;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+@SpringBootTest(classes = GenesisResourcesApplication.class)
 class GenesisResourcesApplicationTests {
 
 	@Test
